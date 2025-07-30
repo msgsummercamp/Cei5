099e98fbd3acd04cb606327ea58adbc4fac7c098
RaresRacsan <rares.racsan@gmail.com>
2025-07-30 15:37:42 +0300

    added connection flight

--- frontend/src/app/shared/components/case-start/case-start.component.ts
+++ frontend/src/app/shared/components/case-start/case-start.component.ts
@@ -15,6 +15,7 @@ import {
   CaseFormComponent,
 } from '../case-form/case-form.component';
 import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-case-start',
@@ -27,6 +28,7 @@ import { InputTextModule } from 'primeng/inputtext';
     ButtonModule,
     InputTextModule,
     CaseFormComponent,
+    MessageModule,
   ],
   templateUrl: './case-start.component.html',
   styleUrl: './case-start.component.scss',
@@ -38,12 +40,26 @@ export class CaseStartComponent {
   public currentStep = 1;
   public isFlightFormValid = false;
   public flightData: FlightDetails | null = null;
+  public maxConnections = 4;
+  public connectionIdCounter = 0;
+  public connectionFlights: ({
+    id: number;
+    valid: boolean;
+    data: FlightDetails | null;
+  } | null)[] = new Array(this.maxConnections).fill(null);
 
   protected readonly reservationForm = this._formBuilder.group({
     reservationNumber: new FormControl<string>(''),
     departingAirport: new FormControl<string>(''),
     destinationAirport: new FormControl<string>(''),
   });
+
+  public onPrevious(prevCallback?: Function) {
+    this.currentStep--;
+    if (prevCallback) {
+      prevCallback();
+    }
+  }
 
   public onNext(nextCallback?: Function) {
     if (this.reservationForm.valid) {
@@ -67,14 +83,77 @@ export class CaseStartComponent {
     }
   }
 
-  public onPrevious(prevCallback?: Function) {
-    this.currentStep--;
-    if (prevCallback) {
-      prevCallback();
+  public onNextFromConnectionFlights(nextCallback?: Function): void {
+    if (this.areAllConnectionFlightsValid()) {
+      this.currentStep++;
+      if (nextCallback) {
+        nextCallback();
+      }
     }
   }
 
   public isMainFlightValid(mainFlightForm: any): boolean {
     return mainFlightForm?.flightDetailsForm?.valid || false;
+  }
+
+  public addConnectionFlight(): void {
+    const emptyIndex = this.connectionFlights.findIndex(
+      (flight) => flight === null
+    );
+    if (emptyIndex !== -1) {
+      this.connectionFlights[emptyIndex] = {
+        id: emptyIndex + 1,
+        valid: false,
+        data: null,
+      };
+    }
+  }
+
+  public canAddMoreConnections(): boolean {
+    return this.connectionFlights.some((flight) => flight === null);
+  }
+
+  public removeConnectionFlight(index: number): void {
+    if (index >= 0 && index < this.connectionFlights.length) {
+      this.connectionFlights[index] = null;
+    }
+  }
+
+  public areAllConnectionFlightsValid(): boolean {
+    const activeFlights = this.connectionFlights.filter(
+      (flight) => flight !== null
+    );
+    if (activeFlights.length === 0) {
+      return true;
+    }
+    // change this after validations are implemented
+    return true;
+  }
+
+  public onConnectionFlightValidityChange(
+    index: number,
+    isValid: boolean,
+    data: FlightDetails | null
+  ): void {
+    if (this.connectionFlights[index]) {
+      this.connectionFlights[index].valid = isValid;
+      this.connectionFlights[index].data = data;
+    }
+  }
+
+  public getActiveConnectionFlights(): {
+    id: number;
+    valid: boolean;
+    data: FlightDetails | null;
+  }[] {
+    return this.connectionFlights.filter(
+      (
+        flight
+      ): flight is {
+        id: number;
+        valid: boolean;
+        data: FlightDetails | null;
+      } => flight !== null
+    );
   }
 }
