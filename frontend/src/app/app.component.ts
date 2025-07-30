aa81b3b90ad956bc691885cd59285ab66ae52f1f
RaresRacsan <rares.racsan@gmail.com>
2025-07-30 17:36:01 +0300

    bs additions

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ButtonModule],
