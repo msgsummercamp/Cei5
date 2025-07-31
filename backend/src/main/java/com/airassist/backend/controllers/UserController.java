100752f252ca2ffe2b4cb60dda7514b60ceeb3eb
AdrianSofariu <adrianradusofariu@gmail.com>
2025-07-31 14:32:42 +0300

    refactor: injected the user mapper into the controller

--- backend/src/main/java/com/airassist/backend/controllers/UserController.java
+++ backend/src/main/java/com/airassist/backend/controllers/UserController.java
@@ -19,41 +19,43 @@ import java.util.UUID;
 public class UserController {
 
     private final UserService userService;
+    private final UserMapper userMapper;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserMapper userMapper) {
         this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID id) throws UserNotFoundException {
         User user = userService.getUserById(id);
-        UserResponseDTO response = UserMapper.INSTANCE.userToUserResponseDTO(user);
+        UserResponseDTO response = userMapper.userToUserResponseDTO(user);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserDTO userDTO) throws DuplicateUserException {
-        User userToCreate = UserMapper.INSTANCE.userDTOToUser(userDTO);
+        User userToCreate = userMapper.userDTOToUser(userDTO);
         User createdUser = userService.addUser(userToCreate);
-        UserResponseDTO response = UserMapper.INSTANCE.userToUserResponseDTO(createdUser);
+        UserResponseDTO response = userMapper.userToUserResponseDTO(createdUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserResponseDTO> updateUser(@PathVariable UUID id, @Valid @RequestBody UserDTO userDTO) throws UserNotFoundException, DuplicateUserException {
-        User userToUpdate = UserMapper.INSTANCE.userDTOToUser(userDTO);
+        User userToUpdate = userMapper.userDTOToUser(userDTO);
         userToUpdate.setId(id);
         User updatedUser = userService.updateUser(userToUpdate);
-        UserResponseDTO response = UserMapper.INSTANCE.userToUserResponseDTO(updatedUser);
+        UserResponseDTO response = userMapper.userToUserResponseDTO(updatedUser);
         return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<UserResponseDTO> patchUser(@PathVariable UUID id, @Valid @RequestBody UserDTO userDTO) throws UserNotFoundException, DuplicateUserException {
-        User userToPatch = UserMapper.INSTANCE.userDTOToUser(userDTO);
+        User userToPatch = userMapper.userDTOToUser(userDTO);
         userToPatch.setId(id);
         User patchedUser = userService.patchUser(userToPatch);
-        UserResponseDTO response = UserMapper.INSTANCE.userToUserResponseDTO(patchedUser);
+        UserResponseDTO response = userMapper.userToUserResponseDTO(patchedUser);
         return ResponseEntity.ok(response);
     }
 
