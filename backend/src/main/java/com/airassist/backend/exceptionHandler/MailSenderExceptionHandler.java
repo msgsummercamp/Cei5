62a269191dc3ab534a53bf4198b591ca45acd752
dimapaul <paul.c.dima@gmail.com>
2025-08-01 11:13:50 +0300

    Changes after review

--- backend/src/main/java/com/airassist/backend/exceptionHandler/MailSenderExceptionHandler.java
+++ backend/src/main/java/com/airassist/backend/exceptionHandler/MailSenderExceptionHandler.java
@@ -3,11 +3,9 @@ package com.airassist.backend.exceptionHandler;
 import jakarta.mail.MessagingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
-@ControllerAdvice
+//#TODO Move mail exception where needed
 public class MailSenderExceptionHandler {
 
     @ExceptionHandler(MessagingException.class)
