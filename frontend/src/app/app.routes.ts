781e69dc8dc4af2d0a4eb6d0f9ceacba264b6676
MargineanMiruna-Elena <miruna.marginean.2018@gmail.com>
2025-08-03 20:38:51 +0300

    landing page updated

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { CaseStartComponent } from './shared/components/case-start/case-start.component';
+import {HomePageComponent} from './shared/components/home-page/home-page.component';
 
 export const routes: Routes = [
   // change these, only for testing purposes
   {
     path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'verify-case',
     redirectTo: '/form',
     pathMatch: 'full',
   },
--- frontend/src/app/shared/components/home-page/home-page.component.ts
+++ frontend/src/app/shared/components/home-page/home-page.component.ts
@@ -1,35 +1,14 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [
+    Button,
+    RouterLink
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent implements OnInit, OnDestroy {
-  headlines: string[] = [
-    'Delayed? Cancelled? Get the compensation you deserve!',
-    'Turn your flight problems into cash.',
-    'Don’t let airlines get away with it, claim your rights!',
-    'Was your flight delayed more than 3 hours?'
-  ];
-
-  currentHeadlineIndex = 0;
-  currentHeadline = this.headlines[0];
-  private intervalId: any;
-
-  ngOnInit(): void {
-    // Change headline every 5 minutes (300000 ms)
-    this.intervalId = setInterval(() => {
-      this.currentHeadlineIndex =
-        (this.currentHeadlineIndex + 1) % this.headlines.length;
-      this.currentHeadline = this.headlines[this.currentHeadlineIndex];
-    }, 300000);
-  }
-
-  ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-}
+export class HomePageComponent {}
