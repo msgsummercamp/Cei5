c154320a1f59c60bace9d154fc3378fcfbd7b28b
MargineanMiruna-Elena <miruna.marginean.2018@gmail.com>
2025-08-05 18:18:33 +0300

    changes based on review

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -9,6 +9,10 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  protected readonly mail = 'airassist.eu@gmail.com';
+  protected readonly phone = '+0123456789';
+  protected readonly socials = 'air.assist';
+
   public scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
