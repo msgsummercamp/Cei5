3decc1a3cb51d0bb13679580c9e2f29f8496b65d
dariuscruceru21 <dariuscruceru@gmail.com>
2025-08-05 09:31:51 +0300

    Solved Cpnflicts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,12 +5,21 @@ import { routes } from './app.routes';
 import Aura from '@primeng/themes/aura';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json',
+        enforceLoading: true,
+      }),
+    }),
     providePrimeNG({
       theme: {
         preset: Aura,
