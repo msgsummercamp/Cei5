e7ea36626dde1b3dc7bd97600df97766b12adbf1
AdrianSofariu <adrianradusofariu@gmail.com>
2025-08-07 17:11:48 +0300

    fix: removed router outlet import from homepage

--- frontend/src/app/features/home-page/home-page.component.ts
+++ frontend/src/app/features/home-page/home-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Button } from 'primeng/button';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { Fieldset } from 'primeng/fieldset';
 import { TranslatePipe } from '@ngx-translate/core';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
@@ -10,15 +10,7 @@ import { NavbarComponent } from '../../shared/components/navbar/navbar.component
 
 @Component({
   selector: 'app-home-page',
-  imports: [
-    Button,
-    RouterLink,
-    Fieldset,
-    TranslatePipe,
-    FooterComponent,
-    RouterOutlet,
-    NavbarComponent,
-  ],
+  imports: [Button, RouterLink, Fieldset, TranslatePipe, FooterComponent, NavbarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
