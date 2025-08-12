3a19d1aa073e0a2743ab64418c7af285846e5f19
dimapaul <paul.c.dima@gmail.com>
2025-08-12 11:39:27 +0300

    Navbar styling

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { LanguageService } from '../../services/language.service';
 import { Language } from '../../types/language';
@@ -9,12 +9,13 @@ import { Button } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { FormsModule } from '@angular/forms';
 import { Select } from 'primeng/select';
-import { NgOptimizedImage } from '@angular/common';
+import { NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
 import { AuthService } from '../../services/auth/auth.service';
 import { IfAuthenticatedDirective } from '../../directives/if-authenticated.directive';
 import { MenuModule } from 'primeng/menu';
 import { UserService } from '../../services/user.service';
 import { StepNavigationService } from '../../services/step-navigation.service';
+import { Accordion, AccordionContent, AccordionHeader, AccordionPanel } from 'primeng/accordion';
 
 @Component({
   selector: 'app-navbar',
@@ -31,6 +32,13 @@ import { StepNavigationService } from '../../services/step-navigation.service';
     IfAuthenticatedDirective,
     Button,
     MenuModule,
+    NgIf,
+    NgForOf,
+    Accordion,
+    AccordionPanel,
+    AccordionHeader,
+    RouterLink,
+    AccordionContent,
   ],
 })
 export class NavbarComponent {
@@ -40,6 +48,8 @@ export class NavbarComponent {
   private readonly _router = inject(Router);
   private readonly _stepNavigationService = inject(StepNavigationService);
   public readonly _languageService = inject(LanguageService);
+
+  public isHamburgerMenuOpen = false;
 
   //Contains the translated elements for the main navbar
   public navbarMainItems: MenuItem[] = [];
@@ -105,6 +115,7 @@ export class NavbarComponent {
   }
 
   public redirectToLogin(): void {
+    this.toggleHamburgerMenu();
     this._stepNavigationService.resetToFirstStep();
     this._router.navigate(['/sign-in']);
   }
@@ -117,5 +128,9 @@ export class NavbarComponent {
   public get userName(): string {
     const userDetails = this._userService.userDetails();
     return userDetails?.firstName || '';
+  }
+
+  public toggleHamburgerMenu(): void {
+    this.isHamburgerMenuOpen = !this.isHamburgerMenuOpen;
   }
 }
