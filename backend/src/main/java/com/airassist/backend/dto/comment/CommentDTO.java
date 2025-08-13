c1fc2d9dc0763846ddc7b23c2521c03272690bcf
RaresRacsan <rares.racsan@gmail.com>
2025-08-13 11:30:16 +0300

    added dtos

---
+++ backend/src/main/java/com/airassist/backend/dto/comment/CommentDTO.java
@@ -0,0 +1,17 @@
+package com.airassist.backend.dto.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDTO {
+    private UUID id;
+    private UUID userId;
+    private String text;
+    private Timestamp timestamp;
+}
