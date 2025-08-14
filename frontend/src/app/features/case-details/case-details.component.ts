d460dbaa448b4c23df00516460d08d7944474916
AdrianSofariu <adrianradusofariu@gmail.com>
2025-08-14 09:24:43 +0300

    feature: continued to work on client details page

--- frontend/src/app/features/case-details/case-details.component.ts
+++ frontend/src/app/features/case-details/case-details.component.ts
@@ -2,17 +2,22 @@ import { Component, inject } from '@angular/core';
 import { CaseService } from '../../shared/services/case.service';
 import { Case } from '../../shared/types/case';
 import { ActivatedRoute } from '@angular/router';
-import { JsonPipe } from '@angular/common';
+import { ProgressSpinner } from 'primeng/progressspinner';
+import { Statuses } from '../../shared/types/enums/status';
+import { TranslateService } from '@ngx-translate/core';
+import { NotificationService } from '../../shared/services/toaster/notification.service';
 
 @Component({
   selector: 'app-case-details',
-  imports: [JsonPipe],
+  imports: [ProgressSpinner],
   templateUrl: './case-details.component.html',
   styleUrl: './case-details.component.scss',
 })
 export class CaseDetailsComponent {
   private readonly _caseService = inject(CaseService);
   private readonly _route = inject(ActivatedRoute);
+  private readonly _translationService = inject(TranslateService);
+  private readonly _notificationService = inject(NotificationService);
 
   public caseData: Case | null = null;
   public loading = true;
@@ -32,5 +37,9 @@ export class CaseDetailsComponent {
     } else {
       this.loading = false;
     }
+  }
+
+  public getStatusTranslation(status: Statuses): string {
+    return this._translationService.instant('statuses.' + status);
   }
 }
